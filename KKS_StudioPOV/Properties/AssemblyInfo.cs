eacd7be6ca4c4152ddbec030b1ff57ba05252ef9
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-11-10 00:11:25 +0100

    Add KKS port

---
+++ KKS_StudioPOV/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KK_StudioPOV")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mantas-2155X aka 2155X")]
+[assembly: AssemblyProduct("KK_StudioPOV")]
+[assembly: AssemblyCopyright("Mantas-2155X aka 2155X Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("30CC0DD3-5FCD-4498-82B9-A896DA739DDD")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(KKS_StudioPOV.KKS_StudioPOV.VERSION)]
+[assembly: AssemblyFileVersion(KKS_StudioPOV.KKS_StudioPOV.VERSION)]
